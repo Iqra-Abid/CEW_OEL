f05d5e0a812a90b2890d4e03c9b36b03633a935f
Iqra-Abid <iqra65952@gmail.com>
2024-11-26 23:08:15 +0500

    Update automate.sh

--- automate.sh
+++ automate.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-
-gcc main.c -o main -lcurl -lcjson
-
-./main
+while true; do
+  gcc main.c -o main -lcurl -lcjson
+  if [ $? -ne 0 ]; then
+    echo "compilation error"
+    exit 1
+  fi
+  ./main
+  sleep 30
+done
